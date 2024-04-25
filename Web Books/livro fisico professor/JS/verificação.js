f3639f012e886aebb5dd3f13baec12cb16809c5a
Victor Hugo Oliveira <74155297+Vickthor1@users.noreply.github.com>
2024-04-25 14:03:16 -0300

    Delete Web Books directory

--- Web Books/livro fisico professor/JS/verificação.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("pedido").addEventListener('click',pedido)
-
-function pedido(){
-    alert("Pedido feito");
-    location.href = "/Web Books/pesquisa professor/pesquisa-prof.html"
-}
--- Web Books/livro fisico/JS/verificação.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("pedido").addEventListener('click',pedido)
-
-function pedido(){
-    alert("Pedido feito");
-    location.href = "/Web Books/pesquisa/pesquisa.html"
-}
