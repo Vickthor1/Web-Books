a97ddb449dce67e128dd3219628ac9408b73936d
Victor Hugo Oliveira <74155297+Vickthor1@users.noreply.github.com>
2024-05-10 20:19:21 -0300

    Delete Web Books directory

--- Web Books/pesquisa professor/JS/adicionar.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("adicionar").addEventListener('click',add)
-
-function add(){
-    alert("Livro adicionado a sua matéria");
-    let estados = document.getElementById('adicionar');
-    estados.remove(estados.selectedIndex);
-}
